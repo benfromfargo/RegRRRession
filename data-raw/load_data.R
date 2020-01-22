86d67f815b2aa13b043d5d7b075d9189843456f7
Ben <bengoehring1@gmail.com>
2020-01-22 11:03:18 -0500

    first commit

---
+++ data-raw/load_data.R
@@ -0,0 +1,8 @@
+library(devtools)
+setwd("/Users/Ben/Box Sync/study/RegRRRession")
+
+load("data-raw/CauchyData.Rda")
+load("data-raw/Pset1CarStopData.Rda")
+load("data-raw/voterList-AgeRace.Rda")
+
+use_data(voterList.subset, x, y, z)
